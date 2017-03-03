952c7bb3832e858188c6745aba5640d7af9cc698
Marcuz Corpuz <macoycorpuz06@gmail.com>
2017-03-04 01:49:45 +0800

    Database Class.

---
+++ UniversalVoting/IDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EXAM_02_Corpuz
+{
+    interface IDatabase
+    {
+        bool HasConnectionError { get; }
+        string ConnectionError { get; }
+
+        DataTable Data { get; }
+
+        void ExecuteCommand(string command);
+
+        void ExecuteStoredProcedure(string command);
+
+        void ExecuteStoredProcedure(string command, List<SqlParameter> sqlParam);
+
+        void Dispose();
+    }
+}
